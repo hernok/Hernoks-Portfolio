2223878d66c9c1fc3072ce71bcbf41ae1b7808a7
hernok <hernok@hotmail.com>
2023-05-02 21:51:14 +0200

    deploying

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hernok.github.io/",
-  build: {
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash][extname]",
-      },
-    },
-  },
+  base: "",
 });
