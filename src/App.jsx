953a3a662eb086cb514fadbc16205371b15c0457
hernok <hernok@hotmail.com>
2023-05-02 17:20:20 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SchoolProjectCard from "./components/cards/SchoolProjectCard/SchoolProjectCard";
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "/src/components/cards/SchoolProjectCard/data.json"
+      );
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div class="card-container">
+      {data.map((card) => (
+        <SchoolProjectCard
+          id={card.id}
+          image={card.image}
+          project_name={card.project_name}
+          date={card.date}
+          description={card.description}
+          group_project={card.group_project}
+          git={card.git}
+          gif={card.gif}
+          alt={card.alt}
+          href={card.href}
+          live={card.live}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/cards/SchoolProjectCard/SchoolProjectCard.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./SchoolProjectCard.module.css";
+
+const SchoolProjectCard = ({
+  id,
+  image,
+  project_name,
+  date,
+  description,
+  group_project,
+  git,
+  gif,
+  alt,
+  href,
+  live,
+}) => {
+  return (
+    <div className={`${styles["school-project-card"]}`} key={`${id}`}>
+      <div className={styles["school-project-card-image"]}>
+        <a
+          href={live ? `${href}` : `${git}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={`${image}`} alt={alt} />
+        </a>
+        <div className={styles["school-project-card-live-container"]}>
+          {live && (
+            <div className={styles["school-project-card-live"]}>
+              <a href={href}>In browser</a>
+            </div>
+          )}
+          {group_project && (
+            <div className={styles["school-project-card-group"]}>
+              Group Project
+            </div>
+          )}
+        </div>
+      </div>
+      <div className={styles["school-project-card-content"]}>
+        <div className={styles["school-project-card-header"]}>
+          <h2 className={styles["school-project-card-title"]}>
+            {project_name}
+          </h2>
+          <p className={styles["school-project-card-date"]}>{date}</p>
+        </div>
+        <div className={styles["school-project-card-description"]}>
+          <p>{description}</p>
+          {git && (
+            <div className={styles["school-project-card-github"]}>
+              <a href={`${git}`} target="_blank" rel="noopener noreferrer">
+                View on GitHub
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SchoolProjectCard;
